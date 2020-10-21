1e9612536452ef1c531a45b93d70f7b7510b6f89
mrigank-newton <68866926+mrigank-newton@users.noreply.github.com>
2020-10-21 19:13:18 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+class App extends Component {
+    constructor(props)
+	{
+		super(props);
+	};
+
+    render() {
+    	return(
+    		<div id="main">
+    			//Do no remove this main div!!
+    			//Make a p tag with id para
+    			//make a button with id click
+    			//Implement functionality on clicking button, the text in para changes form "" 
+    			// to "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    			//and remains the same after first click.(No change on further clicking).
+    		</div>
+    		);
+    }
+}
+
+
+export default App;
+
